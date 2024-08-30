9d139d45001d0ee8ace6f982bd40d8e5eeabbd38
Elmer Miroslav Mosher Golovin <miroslav@mishamosher.com>
2024-08-30 22:56:25 +0300

    Fix 'write_map_to_json.dart' logic.

--- tool/write_map_to_json.dart
+++ tool/write_map_to_json.dart
@@ -1,14 +1,18 @@
 import 'dart:convert';
 import 'dart:io';
-import 'token_map/index.dart';
+import 'package:flutter/widgets.dart';
+import 'package:mishamosher_tiktoken/mishamosher_tiktoken.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await TiktokenDataProcessCenter().initData(ignorePackageName: true);
+
   var map = {
-    'cl100k_base': cl100kBase,
-    'gpt2': gpt2,
-    'p50k_base': p50kBase,
-    'r50k_base': r50kBase,
-    'o200k_base': o200kBase,
+    'cl100k_base': TiktokenDataProcessCenter().cl100kBase,
+    'gpt2': TiktokenDataProcessCenter().gpt2,
+    'p50k_base': TiktokenDataProcessCenter().p50kBase,
+    'r50k_base': TiktokenDataProcessCenter().r50kBase,
+    'o200k_base': TiktokenDataProcessCenter().o200kBase,
   };
 
   for (var key in map.keys) {
@@ -23,6 +27,6 @@ void main() async {
     }
     dir.createSync();
 
-    await File('$key/${key}_$i.json').writeAsString(json.encode(data.entries));
+    await File('$key/${key}.json').writeAsString(json.encode(data.entries));
   }
 }
