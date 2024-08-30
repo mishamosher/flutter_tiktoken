19b0b3da80253ac384aa6ad711db9eceaabe6bfa
Elmer Miroslav Mosher Golovin <miroslav@mishamosher.com>
2024-08-30 22:54:58 +0300

    Refactor 'TiktokenDataProcessCenter'.

--- lib/src/ranks/tiktoken_data_process_center.dart
+++ lib/src/ranks/tiktoken_data_process_center.dart
@@ -18,65 +18,21 @@ class TiktokenDataProcessCenter {
   TiktokenDataProcessCenter._internal();
 
   Future<void> initData({bool ignorePackageName = false}) async {
-    await initCl100kBaseData(ignorePackageName: ignorePackageName);
-    await initGpt2Data(ignorePackageName: ignorePackageName);
-    await initP50kBaseData(ignorePackageName: ignorePackageName);
-    await initR50kBaseData(ignorePackageName: ignorePackageName);
-    await initO200kBaseData(ignorePackageName: ignorePackageName);
+    await initEncodingData(encoding: 'cl100k_base', map: cl100kBase, ignorePackageName: ignorePackageName);
+    await initEncodingData(encoding: 'gpt2', map: gpt2, ignorePackageName: ignorePackageName);
+    await initEncodingData(encoding: 'p50k_base', map: p50kBase, ignorePackageName: ignorePackageName);
+    await initEncodingData(encoding: 'r50k_base', map: r50kBase, ignorePackageName: ignorePackageName);
+    await initEncodingData(encoding: 'o200k_base', map: o200kBase, ignorePackageName: ignorePackageName);
   }
 
-  Future<void> initCl100kBaseData({bool ignorePackageName = false}) async {
+  Future<void> initEncodingData({required String encoding, required Map<String, int> map, bool ignorePackageName = false}) async {
     var prefix = _prefix;
     if (ignorePackageName == true) {
       prefix = '';
     }
-    String jsonString = await rootBundle.loadString('${prefix}lib/tokens/cl100k_base/cl100k_base.json');
+    String jsonString = await rootBundle.loadString('${prefix}lib/tokens/${encoding}/data.json');
     Map<String, dynamic> jsonData = json.decode(jsonString);
     Map<String, int> intData = jsonData.map((key, value) => MapEntry(key, value is int ? value : int.parse(value.toString())));
-    cl100kBase.addAll(intData);
-  }
-
-  Future<void> initGpt2Data({bool ignorePackageName = false}) async {
-    var prefix = _prefix;
-    if (ignorePackageName == true) {
-      prefix = '';
-    }
-    String jsonString = await rootBundle.loadString('${prefix}lib/tokens/gpt2/gpt2.json');
-    Map<String, dynamic> jsonData = json.decode(jsonString);
-    Map<String, int> intData = jsonData.map((key, value) => MapEntry(key, value is int ? value : int.parse(value.toString())));
-    gpt2.addAll(intData);
-  }
-
-  Future<void> initP50kBaseData({bool ignorePackageName = false}) async {
-    var prefix = _prefix;
-    if (ignorePackageName == true) {
-      prefix = '';
-    }
-    String jsonString = await rootBundle.loadString('${prefix}lib/tokens/p50k_base/p50k_base.json');
-    Map<String, dynamic> jsonData = json.decode(jsonString);
-    Map<String, int> intData = jsonData.map((key, value) => MapEntry(key, value is int ? value : int.parse(value.toString())));
-    p50kBase.addAll(intData);
-  }
-
-   Future<void> initR50kBaseData({bool ignorePackageName = false}) async {
-    var prefix = _prefix;
-    if (ignorePackageName == true) {
-      prefix = '';
-    }
-    String jsonString = await rootBundle.loadString('${prefix}lib/tokens/r50k_base/r50k_base.json');
-    Map<String, dynamic> jsonData = json.decode(jsonString);
-    Map<String, int> intData = jsonData.map((key, value) => MapEntry(key, value is int ? value : int.parse(value.toString())));
-    r50kBase.addAll(intData);
-  }
-
-  Future<void> initO200kBaseData({bool ignorePackageName = false}) async {
-    var prefix = _prefix;
-    if (ignorePackageName == true) {
-      prefix = '';
-    }
-    String jsonString = await rootBundle.loadString('${prefix}lib/tokens/o200k_base/o200k_base.json');
-    Map<String, dynamic> jsonData = json.decode(jsonString);
-    Map<String, int> intData = jsonData.map((key, value) => MapEntry(key, value is int ? value : int.parse(value.toString())));
-    o200kBase.addAll(intData);
+    map.addAll(intData);
   }
 }
